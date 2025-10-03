78dfc33fa337525a81879b7744f3fc26887ab82e
Harshit Katheria <123855321+harshuuuuuuuu@users.noreply.github.com>
2025-10-03 16:03:44 +0530

    Delete scripts directory

--- scripts/restart_codedeploy_agent.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Restart CodeDeploy agent on Ubuntu
-sudo systemctl stop codedeploy-agent
-sudo systemctl start codedeploy-agent
-sudo systemctl enable codedeploy-agent
-sudo systemctl status codedeploy-agent
